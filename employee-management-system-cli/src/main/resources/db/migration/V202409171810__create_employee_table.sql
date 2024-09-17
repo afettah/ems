99eb7e533265e58c35c4ad9d56aa79149af403de
afettahghazi <abdelfettah.ghazi@mirakl.com>
2024-09-17 22:37:53 +0200

    add flyway migration and support for postgres and H2

---
+++ employee-management-system-cli/src/main/resources/db/migration/V202409171810__create_employee_table.sql
@@ -0,0 +1,10 @@
+create table "ems"."employees"
+(
+    "id"         uuid                     not null primary key,
+    "email"      text                     not null,
+    "name"       text                     not null,
+    "position"   text                     not null,
+    "salary"     ${jsonType},
+    "created_at" timestamp with time zone not null,
+    "updated_at" timestamp with time zone
+);
