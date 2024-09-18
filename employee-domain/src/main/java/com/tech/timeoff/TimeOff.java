f1ffd07be3d0b851de935b856f9a7992ab470b11
afettahghazi <abdelfettah.ghazi@mirakl.com>
2024-09-18 16:03:12 +0200

    add params validation test

--- employee-domain/src/main/java/com/tech/timeoff/TimeOff.java
+++ employee-domain/src/main/java/com/tech/timeoff/TimeOff.java
@@ -5,6 +5,8 @@ import com.tech.timeoff.category.CategoryId;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @Getter
 public class TimeOff {
@@ -16,6 +18,21 @@ public class TimeOff {
     private TimeOffStatus status;
 
     public static TimeOff create(TimeOffRequest timeOffRequest) {
+        Objects.requireNonNull(timeOffRequest, "Time off request cannot be null");
+
+        if (timeOffRequest.employeeId() == null) {
+            throw new IllegalArgumentException("employeeId cannot be null");
+        }
+        if (timeOffRequest.categoryId() == null) {
+            throw new IllegalArgumentException("categoryId cannot be null");
+        }
+        if (timeOffRequest.dateRange() == null) {
+            throw new IllegalArgumentException("dateRange cannot be null");
+        }
+        if (timeOffRequest.comment() == null) {
+            throw new IllegalArgumentException("comment cannot be null");
+        }
+
         return new TimeOff(TimeOffId.generate(), timeOffRequest.employeeId(), timeOffRequest.categoryId(), timeOffRequest.dateRange(), timeOffRequest.comment(), TimeOffStatus.REQUESTED);
     }
 
--- employee-domain/src/test/java/com/tech/timeoff/TimeOffServiceTest.java
+++ employee-domain/src/test/java/com/tech/timeoff/TimeOffServiceTest.java
@@ -5,12 +5,17 @@ import com.tech.timeoff.category.CategoryId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,6 +47,27 @@ class TimeOffServiceTest {
                 .extracting(TimeOff::getEmployeeId, TimeOff::getCategoryId, TimeOff::getDateRange, TimeOff::getComment, TimeOff::getStatus)
                 .containsExactly(employeeId, categoryId, range, comment, TimeOff.TimeOffStatus.REQUESTED);
         verify(timeOffRepository).create(timeOff);
+    }
 
+    @ParameterizedTest
+    @MethodSource("invalidTimeOffRequest")
+    void request_should_throw_exception_when_required_fields_are_missing(String field, EmployeeId employeeId, CategoryId categoryId, DateRange range, String comment) {
+        //given
+        TimeOffRequest timeOffRequest = new TimeOffRequest(employeeId, categoryId, range, comment);
+
+        //when
+        //then
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> timeOffService.request(timeOffRequest));
+        assertThat(exception).hasMessageContaining(field);
+
+    }
+
+    public static Stream<Arguments> invalidTimeOffRequest() {
+        return Stream.of(
+                Arguments.of("employeeId", null, CategoryId.generate(), new DateRange(LocalDate.now(), LocalDate.now().plusDays(1)), "Time off request comment"),
+                Arguments.of("categoryId", EmployeeId.generate(), null, new DateRange(LocalDate.now(), LocalDate.now().plusDays(1)), "Time off request comment"),
+                Arguments.of("dateRange", EmployeeId.generate(), CategoryId.generate(), null, "Time off request comment"),
+                Arguments.of("comment", EmployeeId.generate(), CategoryId.generate(), new DateRange(LocalDate.now(), LocalDate.now().plusDays(1)), null)
+        );
     }
 }
